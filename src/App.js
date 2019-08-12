fabde02169f73e65bdaef0c44b150f012b60a567
nghiemdao <nghiemvandao@gmail.com>
2019-08-12 15:27:15 +0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Modal />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/ProductList.js
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
+
+class ProductList extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => (
+                    <Product key={product.id} product={product} />
+                  ));
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default ProductList;
